295c7d9ab38927deb274de5e45033471349c68c2
Alex <nartikov@gmail.com>
2019-12-16 21:20:49 +0300

    update
    4

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,9 +1,25 @@
 package main
 
-import "./internal/api/apiserver"
+import (
+
+	"log"
+	"flag"
+	"github.com/Nartikov/goserv/internal/app/apiserver"
+
+)
+
+var (
+	configPath string
+
+)
+
+func init(){
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file for server")
+}
 
 func main(){
-	s := apiserver.New()
+	config := apiserver.NewConfig()
+	s := apiserver.New(config)
 	if err:=s.Start(); err != nil {
 		log.Fatal(err)
 	}
